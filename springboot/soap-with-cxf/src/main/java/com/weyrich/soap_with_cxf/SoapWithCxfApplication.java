320822baa3d8dea478e6c73a3ad8d20be4f54bcd
Jan Weyrich <jweyrich@redhat.com>
2025-07-14 13:49:27 +0200

    initial spring boot service

---
+++ springboot/soap-with-cxf/src/main/java/com/weyrich/soap_with_cxf/SoapWithCxfApplication.java
@@ -0,0 +1,13 @@
+package com.weyrich.soap_with_cxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWithCxfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWithCxfApplication.class, args);
+	}
+
+}
---
+++ springboot/soap-with-cxf/src/test/java/com/weyrich/soap_with_cxf/SoapWithCxfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weyrich.soap_with_cxf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWithCxfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
